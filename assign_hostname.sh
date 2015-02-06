2243bf19f5befad9d06286207cfd5a107ceb1517
un1xer <jameslynnballiet@gmail.com>
2015-02-06 15:02:41 -0800

    adding or updating a script

--- assign_hostname.sh
+++ assign_hostname.sh
@@ -3,15 +3,12 @@
 
 for i in `cat hostsfile`; do
 
-	# GET IP ADDESS...	
-	IPADD=$(echo $i | cut -f 2 -d ':';)
-
 	# GET HOSTNAME...
-	HSTNM=$(echo $i | cut -f 1 -d ':';)
+	 HSTNM=$(echo $i | cut -f 2 -d ':')
 
-	ssh $IPADD "echo $HSTM > /etc/hostname";
-	
-	ssh $IPADD "shutdown -r now";
+	echo $HSTNM > ~/hostname
+	chmod 644 ~/hostname
+	scp ~/hostname $HSTNM:/etc/
 
 done
 
---
+++ gitrun.sh
@@ -0,0 +1,8 @@
+if [ pwd = "~/scripts/ ]
+then
+
+git add *
+git commit -m "adding or updating a script "
+git push origin master
+
+fi
