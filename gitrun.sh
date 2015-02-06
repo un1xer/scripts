d6f78b5d0aba54ec3f57b577e8bc9adb83721ca4
un1xer <jameslynnballiet@gmail.com>
2015-02-06 15:10:57 -0800

    adding or updating a script

--- gitrun.sh
+++ gitrun.sh
@@ -1,8 +1,25 @@
-if [ pwd = "~/scripts/ ]
+CURLOC=$(pwd)
+RSCRIPTS="/root/scripts"
+RDOCS="/root/docs"
+
+if [[ "$CURLOC" == "$RSCRIPTS" ]]
 then
 
-git add *
-git commit -m "adding or updating a script "
-git push origin master
+	echo "RUNNING GIT FOR SCRIPTS REPO"
+	git add *
+	git commit -m "adding or updating a script"
+	git push origin master
+
+elif [[ "$CURLOC" == "$RDOCS" ]]
+then
+
+	echo "RUNNING GIT FOR DOCS REPO"
+	git add *
+	git commit -m "adding or updating a docs"
+	git push origin master
+
+else
+
+	echo "NOT IN EITHER REPO. ENDING NOW"
 
 fi
