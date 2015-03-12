733248f997899684738e1055c649dac8c16763d9
root <root@d1stkfactory>
2015-03-12 00:49:17 +0000

    adding or updating a script

--- copy_file_to_bucket.sh
+++ copy_file_to_bucket.sh
@@ -4,6 +4,12 @@ file=$1
 bucket=$2
 subbucket=$3
 
+if [ "$#" -eq 0 ] || [ "$#" -eq 1 ]; then
+        echo "USAGE: del_file_from_bucket.sh <file2delete> <bucketname> <sub-bucketname>"
+        echo "OPTIONAL: <sub-bucketname> is optional"
+        exit 1;
+fi
+
 if [ "$#" -eq 2 ]; then
 
 	/usr/bin/s3cmd put $file s3://$bucket/
